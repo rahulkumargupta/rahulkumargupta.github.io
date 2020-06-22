00d1bdf2185a844bb9e47742435af63abb8e0809
rgupta <rgupta@travelrepublic.com>
2020-06-22 11:40:55 +0100

    Updates

---
+++ precache-manifest.0644d5dd0c3ceb6c321cee80a9b565ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3812cd23bb24e298effccbeb91916c6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47004655d19e8135483d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "663fac54760b0ff04bc9",
+    "url": "/static/js/2.2887f33b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2887f33b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47004655d19e8135483d",
+    "url": "/static/js/main.dd04944f.chunk.js"
+  },
+  {
+    "revision": "5fa80e98611451dab65a",
+    "url": "/static/js/runtime-main.585dfaba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
